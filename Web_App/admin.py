a065d670233e0428c6341bf2974d51ccb2e9e389
SharvinSunny <sharvinsunny@gmail.com>
2021-10-05 23:24:21 +0530

    added forms

--- Web_App/admin.py
+++ Web_App/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
+
+from Web_App.views import eventform
+
+
 from .models import *
 
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product),
+admin.site.register(Organizer),
+admin.site.register(Storef),
+admin.site.register(Eventform),
+
+
--- Web_App/views.py
+++ Web_App/views.py
@@ -19,6 +19,9 @@ def index(request):
     }
     return render(request, 'Web_App/index.html', context)
 
+def index(request):
+    return render(request, 'Web_App/index.html')
+
 def sports(request):
     return render(request, 'Web_App/sports.html')
 
@@ -31,3 +34,57 @@ def store(request):
     products = Product.objects.all()
     context = {'products':products}
     return render(request, 'Web_App/store.html', context)
+
+def organizer(request):
+    if request.method == "POST":
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        sport = request.POST.get('sport')
+        email = request.POST.get('email')
+        phonenumber = request.POST.get('number')
+        address = request.POST.get('address')
+        code = request.POST.get('code')
+        organizer = Organizer(firstname=firstname, lastname=lastname, sport=sport, email=email,  phonenumber=phonenumber,  address=address,  code=code, ) 
+        organizer.save()
+      
+
+        return HttpResponse("<h1>Thanks<</h1>")
+
+    return render(request, 'Web_App/organizer.html')
+
+def yashlogin(request):
+    return render(request, 'Web_App/yashlogin.html')
+
+def yashregister(request):
+    return render(request, 'Web_App/yashregister.html')
+
+def storef(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        pname = request.POST.get('pname')
+        img = request.POST.get('img')
+        local = request.POST.get('local')
+        desc = request.POST.get('desc')
+        storef = Storef(name=name, pname=pname, img=img, local=local, desc=desc, ) 
+
+        storef.save()
+
+        return HttpResponse("<h1>Thanks<</h1>")
+   
+    return render(request, 'Web_App/storef.html')
+
+def eventform(request):
+     if request.method == "POST":
+        name = request.POST.get('name')
+        date = request.POST.get('date')
+        email = request.POST.get('email')
+        number = request.POST.get('number')
+        local = request.POST.get('local')
+        eventform = Eventform(name=name, date=date, email=email, number=number, local=local, ) 
+
+        eventform.save()
+
+        return HttpResponse("<h1>Thanks<</h1>")
+
+     return render(request, 'Web_App/eventform.html')
+   
