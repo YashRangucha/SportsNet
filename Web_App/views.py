31bf3978bb1bb8a58e8c36bb7b48b3c611e524ca
Sharon005 <sharonts123@gmail.com>
2021-10-06 00:52:10 +0530

    Restarting

--- Web_App/views.py
+++ Web_App/views.py
@@ -8,15 +8,7 @@ API_KEY = '0e55381042014509ade53f0065695efc'
 # Create your views here.
 
 def index(request):
-    url = f'https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey={API_KEY}'
-    response = requests.get(url)
-    data = response.json()
-    articles = data['articles']
-
-
-    context = {
-        'articles' : articles
-    }
+    
     return render(request, 'Web_App/index.html', context)
 
 def index(request):
